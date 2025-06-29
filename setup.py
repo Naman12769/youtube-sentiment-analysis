bc748bf4f806f39171964f006a82255d6beca4f5
Naman12769 <namanjainjain341@gmail.com>
2025-06-29 16:41:08 +0530

    Your commit message

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+setup(
+  name="youtube",
+  version="0.0.0",
+  author="Naman",
+  author_email="naman@gmail.com",
+  package=find_packages(),
+  install_requires=[]
+
+)
